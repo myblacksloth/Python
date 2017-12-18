a9dc16959edb90cbcbee0311ed28101c7d0ca9e1
Antonio Maulucci <antonio.maulucci@libero.it>
2017-12-18 18:45:41 +0100

    Create main.py

---
+++ classes/main.py
@@ -0,0 +1,23 @@
+import fun
+#from fun import newclass
+
+
+ff = fun.newclass()
+
+
+
+z = fun.__sum__(4, 3)
+
+k = fun.__mult__(3,4)
+
+e = fun.__pot__(2,3)
+
+ff.setter(1,3)
+
+j = ff.__rest__(10,2)
+
+print(z)
+print(k)
+print(e)
+print(j)
+ff.__toString__()
