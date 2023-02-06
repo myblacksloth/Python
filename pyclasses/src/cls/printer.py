7c098631c0674766bbed8677aad53113ef8a1e9c
Antonio Maulucci <25264956+myblacksloth@users.noreply.github.com>
2023-02-06 12:24:48 +0100

    Add files via upload

--- pyclasses/src/cls/printer.py
+++ pyclasses/src/cls/printer.py
@@ -20,3 +20,15 @@ class plotter:
         
         def getType(self):
                 return self.tipo
+
+class laserPlotter(plotter): # e' una sottoclasse di plotter
+        def __init__(self, custom=None):
+                super().__init__(custom)
+                self.laserType = True
+                if self.tipo != "laser":
+                        self.tipo = self.tipo+"__laserType"
+
+        def isLaser(self):
+                # if self.laserType:return True
+                # else: return False
+                return self.laserType
--- pyclasses/src/main.py
+++ pyclasses/src/main.py
@@ -1,17 +1,21 @@
 
-from src.cls.printer import printer, plotter
+from src.cls.printer import printer, plotter, laserPlotter
 
 def main():
 
         pr = printer() # nuovo oggetto
         print("l'oggetto pr e' di tipo printer? {}".format(isinstance(pr, printer)))
-
         pr.printTxt("hello")
 
         cdata = {}
         cdata['tipo']='plotter'
+        
         pl = plotter(custom=cdata)
         print("il tipo di pl e' {}".format(pl.getType()))
+
+        lpl = laserPlotter(custom=cdata)
+        print("il tipo di lpl e' {}".format(lpl.getType()))
+
 
 
 if __name__ == "__main__":
